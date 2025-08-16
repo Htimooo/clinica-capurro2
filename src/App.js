c308cd08a4c2a10ccd0a50ee376583b8c468f30d
Hugo Andrés Timote <76624227+Htimooo@users.noreply.github.com>
2025-08-16 20:18:20 -0300

    refactor: lazy load page routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Blog from "./pages/Blog";
-import Financiamiento from "./pages/Financiamiento";
-import Contacto from "./pages/Contacto";
+import { lazy, Suspense } from "react";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Tratamiento from "./pages/Tratamiento";
-import LandingOrtodoncia from "./pages/LandingOrtodoncia";
-import LandingOrtopedia from "./pages/LandingOrtopedia";
-import Articulo from "./pages/Articulo";
 import ScrollToTop from "./components/ScrollToTop";
 import WhatsAppButton from "./components/WhatsAppButton";
 import ContactForm from "./components/ContactForm";
-import NotFound from "./pages/NotFound";
+
+const Home = lazy(() => import("./pages/Home"));
+const Blog = lazy(() => import("./pages/Blog"));
+const Financiamiento = lazy(() => import("./pages/Financiamiento"));
+const Contacto = lazy(() => import("./pages/Contacto"));
+const Tratamiento = lazy(() => import("./pages/Tratamiento"));
+const LandingOrtodoncia = lazy(() => import("./pages/LandingOrtodoncia"));
+const LandingOrtopedia = lazy(() => import("./pages/LandingOrtopedia"));
+const Articulo = lazy(() => import("./pages/Articulo"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
  
 
@@ -30,18 +32,20 @@ function App() {
       <ScrollToTop />
      {/*  <WhatsAppButton /> */}
       
-        <Navbar />     
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path= '/ortodoncia' element={<LandingOrtodoncia/>}/>
-          <Route path= '/ortopedia' element={<LandingOrtopedia/>}/>
-          <Route path= '/blog/:ruta' element={<Articulo/>}/>
-          <Route path= '/blog' element={<Blog/>}/>
-          <Route path= '/tratamiento/:ruta' element={<Tratamiento/>}/>
-          <Route path= '/financiamiento' element={<Financiamiento/>}/>
-          <Route path= '/contacto' element={<Contacto/>}/>          
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Navbar />
+        <Suspense fallback={<div>Cargando...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path='/ortodoncia' element={<LandingOrtodoncia/>}/>
+            <Route path='/ortopedia' element={<LandingOrtopedia/>}/>
+            <Route path='/blog/:ruta' element={<Articulo/>}/>
+            <Route path='/blog' element={<Blog/>}/>
+            <Route path='/tratamiento/:ruta' element={<Tratamiento/>}/>
+            <Route path='/financiamiento' element={<Financiamiento/>}/>
+            <Route path='/contacto' element={<Contacto/>}/>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
         <ContactForm/>
         <Footer />
       </Router>      
